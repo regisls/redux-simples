5c5523787ac686c83f6ddd3c20973612db2c161a
Regis Leandro Sebastiani <regisls@gmail.com>
2022-08-23 09:22:55 -0300

    Commit inicial do exercício Redux-Simples

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Exercício React-Redux (Simples)</h1>
+      <div className='linha'>
+        <Card title='Teste' red>adfadf</Card>
+        <Card title='Teste' green>adfadf</Card>
+      </div>
+      <div className='linha'>
+        <Card title='Outro' blue>aaaaa</Card>
+        <Card title='Outro' purple>aaaaa</Card>
+      </div>
     </div>
   );
 }
